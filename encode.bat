f238ae1da16fd220bad2097a8786be48379d5fdd
Vladyslav Shynkarenko <vadic7wee2@gmail.com>
2012-06-19 18:45:55 +0300

    added i18n support

---
+++ encode.bat
@@ -0,0 +1,2 @@
+rem this file is used to encode russian characters to ascii codes in the bundle file
+native2ascii -encoding utf8 ./src/main/resources/messages_ru_origin.properties ./src/main/resources/messages_ru.properties
